d1e371fb0ae5a5d0c602abb22071f8a68c446067
luukdg <luukdegraaf1993@gmail.com>
2025-06-08 15:41:53 +0200

    added styling

--- javascript.js
+++ javascript.js
@@ -1,1 +1,28 @@
-console.log("Hello World")
+// Basis variables
+let firstNumber = 10;
+let secondNumber = 20;
+let result = 0;
+
+// Functions for add, subtract, multiply and divide
+function add(a, b) {
+    return result = a + b;
+}
+
+function subtract(a, b) {
+    return result = a - b;
+}
+
+function multiply(a, b) {
+    return result = a * b;
+}
+
+function divide(a, b) {
+    return result = a / b;
+}
+
+// Function to pick the right operator
+function operate(a, b, operatorFunction) {
+    return operatorFunction(a, b);
+};
+
+console.log(operate(firstNumber, secondNumber, divide));
